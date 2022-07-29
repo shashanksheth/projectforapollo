5de80cc841976de03a3577cd3684d3938f9a5a2c
Shashank Sheth <shashanksheth@Shashanks-Personal-Macbook.local>
2022-07-28 23:01:40 -0400

    updating load_trades_data to account for floating point errors

--- Part 1/load_trades_data.py
+++ Part 1/load_trades_data.py
@@ -32,13 +32,16 @@ fill_df.columns = fill_header
 
 # order_df formatting
 order_df = order_df.astype({"Order Number": int, "Amount": float, "Filled Amount": float})
+order_df = order_df.round({'Amount': 2, 'Filled Amount': 2})
 
 # route_df formatting
 route_df = route_df.astype({"Order Number": int, "Route Number": int, "Routed Amount": float, "Route Filled Amount": float, "Route Avg Price": float, "Route Comm Amount": float, "Route Comm Rate": float})
+route_df = route_df.round({'Routed Amount': 2, 'Route Filled Amount': 2, 'Route Avg Price': 2, 'Route Comm Amount': 2, 'Route Comm Amount': 2,})
 
 # fill_df formatting
 fill_df = fill_df.astype({"Order Number": int, "Route Number": int, "Fill Amount": float, "Fill Price": float, "Exec Seq Num": int, "Prev Exec Seq Num": float})
 fill_df = fill_df.astype({"Prev Exec Seq Num": int})
+fill_df = fill_df.round({'Fill Amount': 2, 'Filled Price': 2})
 
 # create db engine
 trades_db = ce('sqlite:///trades.sqlite')
